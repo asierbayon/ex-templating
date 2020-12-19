591ab961c62cbaae10df2946600d8f91cdfa7d6e
asierbayon <bayonasier@gmail.com>
2020-12-19 14:40:14 +0100

    iteration 3 done

--- controllers/tweets.controller.js
+++ controllers/tweets.controller.js
@@ -1,7 +1,17 @@
 // Iteration 3: import tweets data
+const tweetsDB = require('../data/tweets')
 
 // Iteration 3: list tweets from file
 module.exports.list = (req, res, next) => {
+  const { user } = req.query;
+
+  const tweets = tweetsDB
+  
+  res.render('tweets/list', {
+    tweets: tweets,
+    user: user
+  });
+
   // Order tweets desc by date
   // Iteration 4: filter tweets by user checking the query param 'name'
 }
